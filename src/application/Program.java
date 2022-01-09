f470602034dc6d82c6be450f0b06f44010de8401
Kazcarob <83021307+Cau312@users.noreply.github.com>
2022-01-09 19:49:42 -0300

    Adicionado Classe Seller
    Só oq tá no titulo memo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.Date;
+
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -12,7 +15,13 @@ public class Program {
 		DB.closeConnection();
 
 		Department dptM = new Department(1, "Macacos");
+		
+		Seller vend = new Seller(21,"Marquinhos", "xaolinmatadordeporco@gmail.com", new Date() , 5000.0, dptM);
+		
 		System.out.println(dptM);
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println(vend);
 
 	}
 

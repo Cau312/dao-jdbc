d81eae8768d06b62d091db38e0a52fe1bf0c4e6b
Kazcarob <83021307+Cau312@users.noreply.github.com>
2022-01-19 22:00:49 -0300

    Implementado a busca por id (Permanentemente)
    É oq tá no titulo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,8 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.Scanner;
-
 import db.DB;
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
@@ -14,48 +10,13 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner sc = new Scanner(System.in);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+
+		Seller seller = sellerDao.findById(3);
+
+		System.out.println(seller);
 		
-		int id = 0;
-		
-		Connection conn = DB.getConnection();
-		Statement st = null;
-		ResultSet rs = null;
-		
-		try {
-
-			st = conn.createStatement();
-
-			
-			System.out.println("Digite o Id de quem procura");
-			id = sc.nextInt();
-			
-			
-			rs = st.executeQuery("SELECT seller.*,department.Name as DepName \r\n"
-					+ "FROM seller INNER JOIN department \r\n"
-					+ "ON seller.DepartmentId = department.Id \r\n"
-					+ "WHERE seller.Id = " + id);
-
-			Seller sel = null;
-					
-			while (rs.next() == true) {
-
-				sel = new Seller(rs.getInt("Id"), rs.getString("Name"), rs.getString("Email"),
-						rs.getDate("BirthDate"), rs.getDouble("BaseSalary"),
-						new Department(rs.getInt("DepartmentId"), rs.getString("DepName")));
-			}
-			
-			System.out.println(sel);
-			
-			
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}finally {
-			DB.closeResultSet(rs);
-			DB.closeStatement(st);
-			DB.closeConnection();
-			sc.close();
-		}
+		DB.closeConnection();
 	}
 
 }
